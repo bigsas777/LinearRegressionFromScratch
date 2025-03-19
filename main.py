d3ae212dc17d63ecf8d091061abfe4cbb2c6ff5f
bigsas777 <luca.zorzi77@gmail.com>
2025-03-19 22:50:19 +0100

    meme seeds and spacing

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ import time
 from math import sqrt
 
 # Generazione dataset di training
-np.random.seed(42)
+np.random.seed(420)
 input = np.random.rand(100, 1) * 10
 noise = np.random.randn(100, 1) * 2  
 output = 3 * input + 7 + noise
 
 # Generazione dataset di test
-np.random.seed(99)
+np.random.seed(777)
 input_test = np.random.rand(100, 1) * 10  
 noise_test = np.random.randn(100, 1) * 2  
 output_test = 3 * input_test + 7 + noise_test
+
 
 # Creazione e addestramento modello
 model = LinearRegression(0.01, 1000)
@@ -24,19 +25,20 @@ model.train(input, output)
 end = time.perf_counter()
 theta0, theta1 = model.theta0, model.theta1
 
-
 # Metriche del modello
 train_time = end - start
 test_pred = model.predict(input_test)
 r2 = r2_score(output_test, test_pred)
 mae = mean_absolute_error(output_test, test_pred)
 mse = mean_squared_error(output_test, test_pred)
+
 # Stampa metriche
 print(f"Training time: {train_time}s")
 print(f"Theta0: {theta0}, Theta1: {theta1}")
 print(f"R2 Score: {r2}")
 print(f"Mean Absolute Error (MAE): {mae}")
 print(f'Root Mean Squared Error: {sqrt(mse)}')
+
 
 # Scatter plot con retta regressione
 plt.figure(figsize=(8,6))
