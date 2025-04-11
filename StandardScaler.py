34d1a6ac4127d148bc593f6064ec7fb19f538f54
bigsas777 <luca.zorzi77@gmail.com>
2025-04-11 22:35:31 +0200

    Removed main.py (useless)

--- StandardScaler.py
+++ StandardScaler.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 class StandardScaler():
-    def __init__(self, means=None, std_devs=None):
-        self.means = means
-        self.std_devs = std_devs
+    def __init__(self):
+        self.means = 0.
+        self.std_devs = 0.
 
     def fit(self, input_data):
         self.means = np.mean(input_data, axis=0)
@@ -14,7 +14,7 @@ class StandardScaler():
 
     def fit_transform(self, input_data):
         self.fit(input_data)
-        return self.transform()
+        return self.transform(input_data)
 
     def inverse_transform(self, normalized_data):
         return normalized_data * self.std_devs + self.means
